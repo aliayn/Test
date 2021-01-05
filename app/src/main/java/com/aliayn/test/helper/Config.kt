064f053b0ac91a7d442a9402ba702d191595697d
Ali Aynechian <aliaynechiyan@gmail.com>
2021-01-05 13:52:56 +0330

    add pref name

--- app/src/main/java/com/aliayn/test/helper/Config.kt
+++ app/src/main/java/com/aliayn/test/helper/Config.kt
@@ -1,6 +1,11 @@
 package com.aliayn.test.helper
 
 import com.chibatching.kotpref.KotprefModel
+import com.chibatching.kotpref.blockingBulk
 
 object Config : KotprefModel() {
+
+    override val kotprefName: String = "Prefs"
+
+    fun reset(): Unit = blockingBulk { clear() }
 }
